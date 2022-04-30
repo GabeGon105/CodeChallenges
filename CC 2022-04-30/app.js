ff1f7dfdc21d175420f68ff393cdbd0ca082b70d
Gabe Gonzalez <gabegon105@gmail.com>
2022-04-30 14:34:10 +0900

    Coding Challenge and solution for 2022-04-30 coding challenge.

---
+++ CC 2022-04-30/app.js
@@ -0,0 +1,21 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+    let final ='';
+    
+    for ( let i = 0 ; i < s.length; i++ ) {
+      final = final + s[i].toUpperCase();
+      for ( let j = 0 ; j < i ; j++ ) {
+        final += s[i].toLowerCase()
+      }
+      final += '-'
+    }
+    return final.slice(0, -1);
+  }
