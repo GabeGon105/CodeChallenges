3665594510c6ac5f4373445bf20ed3df441a6d45
Gabe Gonzalez <gabegon105@gmail.com>
2022-08-29 08:27:21 +0900

    Coding Challenge and solution for 2022-08-29 coding challenge.

---
+++ CC 2022-08-29/app.js
@@ -0,0 +1,19 @@
+// Implement a function which multiplies two numbers.
+
+
+
+// Parameters: two integers
+
+// Return: one integer
+
+// Examples:
+multiply( 2, 3 ) // 6
+multiply( 5, 10 ) // 50
+multiply( 7, 1 ) // 7
+
+
+
+function multiply( num1, num2 ) {
+    // return num1 multiplied by num2
+    return num1 * num2;
+  }
