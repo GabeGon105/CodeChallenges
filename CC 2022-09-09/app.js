e146c4e37227bd1c92351e385f77b8f9c00b068a
Gabe Gonzalez <gabegon105@gmail.com>
2022-09-09 16:49:47 +0900

    Coding Challenge and solution for 2022-09-09 coding challenge.

---
+++ CC 2022-09-09/app.js
@@ -0,0 +1,22 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+
+
+// Parameters: 
+
+// Return: 
+
+// Examples: 
+nameShuffler('jupiter Volcana') // 'Volcana jupiter'
+nameShuffler('boony Hoony') // 'Hoony boony'
+nameShuffler('tom jerry') // 'jerry tom'
+
+
+
+function nameShuffler(str){
+    // split parameter string by spaces to an array and reverse
+    let arr = str.split(' ').reverse();
+    
+    // join arr to a str with a space between and return
+    return arr.join(' ');
+}
