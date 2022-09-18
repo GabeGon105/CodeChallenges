3db105e834fe331f10a9a7769999ad5bce1a7b88
Gabe Gonzalez <gabegon105@gmail.com>
2022-09-18 23:06:41 +0900

    Coding Challenge and solution for 2022-09-18 coding challenge.

--- CC 2022-09-18/app.js
+++ CC 2022-09-18/app.js
@@ -1,6 +1,6 @@
 // Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
 
-// Return as a number.
+// Return as a number. 
 
 
 
@@ -32,4 +32,4 @@ function divCon(x){
     } );
     
     return sum;
-} 
+}
