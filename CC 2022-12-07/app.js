8803f1c5054e4283dc33b76d7c8b901f8b98a647
gabe <gabegon105@gmail.com>
2022-12-07 09:00:42 +0900

    Coding Challenge and solution for 2022-12-07 coding challenge.

---
+++ CC 2022-12-07/app.js
@@ -0,0 +1,18 @@
+// Write reverseList function that simply reverses lists.
+
+
+
+// Parameters: an array, may be empty
+
+// Return: the argument array reversed
+
+// Examples:
+reverseList([]) // []
+reverseList([1,2,3]) // [3,2,1]
+
+
+
+const reverseList = (list) => {
+    // reverse the argument list and return
+    return list.reverse();
+}
