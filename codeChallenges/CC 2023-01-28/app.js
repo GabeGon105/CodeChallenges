1c2e06cdf722355aabfdf919937fdee321723ab8
gabe <gabegon105@gmail.com>
2023-01-28 09:18:16 +0900

    Coding Challenge and solution for 2023-01-28 coding challenge.

---
+++ codeChallenges/CC 2023-01-28/app.js
@@ -0,0 +1,48 @@
+// Create a function that takes two arguments:
+
+// An array of objects which feature the season, the team and the country of the Champions League winner.
+
+// Country (as a string, for example, 'Portugal')
+
+// You function should then return the number which represents the number of times a team from a given country has won. Return 0 if there have been no wins.
+
+
+
+// Parameters: an array of objects which feature the season, the team and the country of the Champions League winner, and a string representing a country
+
+// Return: an integer representing the number of wins from the argument country string
+
+//Exampes:
+const winnerList1 = [
+    { season: '1996–97', team: 'Borussia Dortmund', country: 'Germany' },
+    { season: '1997–98', team: 'Real Madrid', country: 'Spain' },
+    { season: '1998–99', team: 'Manchester United', country: 'England' },
+    { season: '1999–00', team: 'Real Madrid', country: 'Spain' },
+    { season: '2000–01', team: 'Bayern Munich', country: 'Germany' },
+    { season: '2001–02', team: 'Real Madrid', country: 'Spain' },
+    { season: '2002–03', team: 'Milan', country: 'Italy' },
+    { season: '2003–04', team: 'Porto', country: 'Portugal' },
+    { season: '2004–05', team: 'Liverpool', country: 'England' },
+    { season: '2005–06', team: 'Barcelona', country: 'Spain' },
+    { season: '2006–07', team: 'Milan', country: 'Italy' },
+    { season: '2007–08', team: 'Manchester United', country: 'England' },
+    { season: '2008–09', team: 'Barcelona', country: 'Spain' },
+    { season: '2009–10', team: 'Internazionale', country: 'Italy' },
+    { season: '2010–11', team: 'Barcelona', country: 'Spain' },
+    { season: '2011–12', team: 'Chelsea', country: 'England' },
+    { season: '2012–13', team: 'Bayern', country: 'Germany' },
+    { season: '2013–14', team: 'Real Madrid', country: 'Spain' },
+    { season: '2014–15', team: 'Barcelona', country: 'Spain' },
+    { season: '2015–16', team: 'Real Madrid', country: 'Spain' }
+  ];
+  
+countWins(winnerList1, 'Portugal') // 1
+countWins(winnerList1, 'FootLand') // 0
+countWins(winnerList1, 'Spain') // 9
+
+
+
+const countWins = ( winnerArr , countryStr ) => {
+    // filter through winnerArr for object.country === countryStr, then return the length
+    return winnerArr.filter( obj => obj.country === countryStr ).length
+}
