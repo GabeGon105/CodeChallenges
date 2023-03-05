9bb85c8c5374c6b02eab72cc918f0cc7fd557c24
gabe <gabegon105@gmail.com>
2023-03-05 11:44:01 +0900

    Coding Challenge and solution for 2023-03-05 coding challenge.

---
+++ codeChallenges/CC 2023-03-05/app.js
@@ -0,0 +1,34 @@
+// Create a function, called insurance(), that computes the cost of renting a car. The function takes 3 arguments: the age of the renter, the size of the car, and the number days for the rental. The function should return an integer number of the calculated total cost of the rental.
+
+// Age (integer) : There is a daily charge of $10 if the driver is under 25
+
+// Car Size (string) : There may be an additional daily charge based on the car size:
+
+// car size surcharge "economy" $0 "medium" $10 "full-size" $15
+
+// Rental Days (integer) : There is a base daily charge of $50 for renting a car. Simply multiply the one day cost by the number of days the car is rented in order to get the full cost.
+
+// Note: Negative rental days should return 0 cost. Any other car size NOT listed should come with a same surcharge as the "full-size", $15.
+
+
+
+// Parameters: 3 arguments, integer age of the driver, string car size, integer rental days
+
+// Return: an integer representing the total cost
+
+// Examples:
+insurance(18, "medium", 7); // => 490
+insurance(30,"full-size",30); // => 1950
+insurance(21,"economy",-10); // => 0
+insurance(42,"my custom car",7); // => 455
+
+
+
+const insurance = ( age, car, days ) => {
+    // calculate the Cost of the driver's age, and the Cost for the car size, then save to variables
+    const ageCost = age < 25 ? 10 : 0;
+    const sizeCost = car === "economy" ? 0 : ( car === "medium" ? 10 : 15 );
+
+    // if argument days is less than 0 return 0, else return 50 plus ageCost plus sizeCost times argument days
+    return days < 0 ? 0 : ( 50 + ageCost + sizeCost ) * days;
+}
