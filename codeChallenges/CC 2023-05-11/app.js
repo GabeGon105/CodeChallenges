3992303b440a8bf78c765314cc580ced3b57af12
Gabe Gonzalez <gabegon105@gmail.com>
2023-05-11 00:00:55 +0900

    Coding Challenge and solution for 2023-05-11 coding challenge.

---
+++ codeChallenges/CC 2023-05-11/app.js
@@ -0,0 +1,32 @@
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+
+
+// Parameters: an array of integers
+
+// Return: an integer representing the integer from the argument array that appears an odd number of times
+
+// Examples:
+findOdd([7]) // 7
+findOdd([0]) // 0
+findOdd([1,1,2]) // 2
+findOdd([0,1,0,1,0]) // 0
+findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]) // 4
+
+
+
+const findOdd = (arr) => {
+    // filter through arr to find only the numbers that appear an odd number of times in arr, then return the first value
+    return arr.filter( (num) => {
+        return arr.filter( (int) => int === num ).length % 2 !== 0;
+    } )[0]
+}
