159135ae0737ef43845c1e52e6e4d86e36e04e29
Gabe Gonzalez <gabegon105@gmail.com>
2023-07-27 13:22:13 -0400

    Coding Challenge and solution for 2023-07-27 coding challenge.

---
+++ codeChallenges/CC 2023-07-27/app.js
@@ -0,0 +1,20 @@
+// For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
+
+// Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array. Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+
+
+
+// Parameter: an array of integers representing the letters of the alphabet
+
+// Return: "WIN" if the argument array contains all the integer values that represent "bingo", else return "LOSE" 
+
+// Examples:
+bingo([1,2,3,4,5,6,7,8,9,10]) // "LOSE"
+bingo([21,13,2,7,5,14,7,15,9,10]) // "WIN"
+
+
+
+const bingo = (arr) => {
+    // create an array to hold the numbers associated with "bingo", check if every value is included in argument arr, return "WIN" if true else return "LOSE"
+    return [2,9,14,7,15].every( (num) => arr.includes(num) ) ? "WIN" : "LOSE";
+}
