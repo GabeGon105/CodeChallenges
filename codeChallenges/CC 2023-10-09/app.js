20a8a63f628e6114741b380fea0a50b6939022bc
Gabe Gonzalez <gabegon105@gmail.com>
2023-10-09 11:33:39 -0400

    Coding Challenge and solution for 2023-10-09 coding challenge.

---
+++ codeChallenges/CC 2023-10-09/app.js
@@ -0,0 +1,24 @@
+// You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. You can, however, use the String constructor and any related functions.
+
+// You cannot use the following:
+// "Hello, World!"
+// 'Hello, World!'
+// `Hello, World!`
+
+// Good luck and try to be as creative as possible!
+
+
+
+// Parameters: no arguments
+
+// Return: the string 'Hello, World!' without using quotes, double quotes, or template strings
+
+// Example:
+hellowWorld() // "Hello, World!"
+
+
+
+const helloWorld = () => {
+    // use the String.fromCharCode constructor to build the target string from each individual character code
+    return String.fromCharCode( 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
+}
