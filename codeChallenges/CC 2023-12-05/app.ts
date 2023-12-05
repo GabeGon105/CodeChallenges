9658cce5c122cdd466c074724c36a7e5e496470f
Gabe Gonzalez <gabegon105@gmail.com>
2023-12-05 13:07:59 -0500

    Coding Challenge and solution for 2023-12-05 coding challenge.

---
+++ codeChallenges/CC 2023-12-05/app.ts
@@ -0,0 +1,26 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+// Your function should only return a number, not the explanation about how you get that number.
+
+
+
+export function getSum(a: number, b: number): number {
+  let sum = 0;
+
+  // loop from the min value a or b to the max value a or b
+  // add each value to sum
+  for ( let i = Math.min(a,b) ; i <= Math.max(a,b) ; i++ ) {
+    sum += i;
+  }
+
+  return sum;
+}
